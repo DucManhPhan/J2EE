c4ed30d22d3aaa73d19b7bc7821d1b8843cce6bc
vietlt95 <lethevietmta@gmail.com>
2019-08-20 17:45:18 +0700

    update files

---
+++ src/libs/SearchUtils/src/Trie_search/HashArray.java
@@ -0,0 +1,29 @@
+package Trie_search;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * https://www.npmjs.com/package/hasharray
+ * https://github.com/joshjung/hash-array
+ * http://myjavaadventures.com/blog/2017/06/12/java-8-predicates-to-pass-methods-as-arguments/
+ * 
+ * @author admin
+ *
+ */
+public class HashArray {
+
+	private List<String[]> keyFields;
+	
+	private Predicate callback;
+	
+	private String options;
+	
+	/**
+	 * Insertion
+	 */
+	
+	public void add() {
+		
+	}
+}
---
+++ src/libs/SearchUtils/src/Trie_search/TrieSearch.java
@@ -0,0 +1,12 @@
+package Trie_search;
+
+
+/**
+ * https://github.com/joshjung/trie-search/blob/master/src/TrieSearch.js
+ * 
+ * @author admin
+ *
+ */
+public class TrieSearch {
+
+}

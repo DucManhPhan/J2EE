2b7be63f210261a6e9ed92db158edd9a09c5e1e4
ManhPD5 <ducmanhphan93@gmail.com>
2019-10-15 13:23:40 +0700

    update files

--- src/Java_Core/JavaFX/src/Basic-JavaFX/src/main/java/com/manhpd/hello_world/HelloWorldApp.java
+++ src/Java_Core/JavaFX/src/Basic-JavaFX/src/main/java/com/manhpd/hello_world/HelloWorldApp.java
@@ -1,4 +1,19 @@
 package com.manhpd.hello_world;
 
-public class HelloWorldApp {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class HelloWorldApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(new Pane(), 800, 600));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }

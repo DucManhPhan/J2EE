7ea6fbf4d69ac1545af57d9a9e6d29f04b0f0f7c
ManhPD5 <ducmanhphan93@gmail.com>
2019-10-16 07:39:50 +0700

    update files

--- src/Java_Core/JavaFX/src/Basic-JavaFX/src/main/java/com/manhpd/hello_world/HelloWorldApp.java
+++ src/Java_Core/JavaFX/src/Basic-JavaFX/src/main/java/com/manhpd/hello_world/HelloWorldApp.java
@@ -2,15 +2,30 @@ package com.manhpd.hello_world;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+
+/**
+ * https://openjfx.io/openjfx-docs/
+ *
+ */
 public class HelloWorldApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(new Pane(), 800, 600));
-        stage.show();
+        try {
+            BorderPane root = new BorderPane();
+            Scene scene = new Scene(root, 800, 400);
+//            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+//        stage.setScene(new Scene(new Pane(), 800, 600));
+//        stage.show();
     }
 
     public static void main(String[] args) {

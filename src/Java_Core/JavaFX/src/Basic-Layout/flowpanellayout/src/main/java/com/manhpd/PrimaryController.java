821903f402cbe817a8eb65235ac73a42ba57e5d8
ManhPD5 <ducmanhphan93@gmail.com>
2019-10-20 22:37:49 +0700

    update files

---
+++ src/Java_Core/JavaFX/src/Basic-Layout/flowpanellayout/src/main/java/com/manhpd/PrimaryController.java
@@ -0,0 +1,12 @@
+package com.manhpd;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        System.out.println("Switch to secondary secene");
+    }
+}

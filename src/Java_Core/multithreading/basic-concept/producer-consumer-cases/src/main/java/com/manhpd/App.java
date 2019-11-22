bda46a38a71aa07db040fad64479e88f9ee61308
ManhPD5 <ducmanhphan93@gmail.com>
2019-11-23 02:23:47 +0700

    update files

---
+++ src/Java_Core/multithreading/basic-concept/producer-consumer-cases/src/main/java/com/manhpd/App.java
@@ -0,0 +1,12 @@
+package com.manhpd;
+
+
+import com.manhpd.using_blocking_queue.HighThroughputMain;
+
+public class App {
+
+    public static void main(String[] args) {
+        HighThroughputMain.start();
+    }
+
+}
---
+++ src/Java_Core/multithreading/basic-concept/producer-consumer-cases/src/main/java/com/manhpd/using_blocking_queue/Consumer.java
@@ -0,0 +1,29 @@
+package com.manhpd.using_blocking_queue;
+
+public class Consumer implements Runnable {
+
+    private String name;
+
+    private Broker broker;
+
+    public Consumer(String name, Broker broker) {
+        this.name = name;
+        this.broker = broker;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Integer data = broker.get();
+            while (this.broker.continueProducing || data != null) {
+                Thread.sleep(1000);
+                System.out.println("Consumer " + this.name + " processed data from broker: " + data);
+                data = broker.get();
+            }
+
+            System.out.println("Consumer " + this.name + " terminating its job --> terminating.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

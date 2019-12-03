327c09ffce0da2fe0b2a372c9baee971874ada19
ManhPD5 <ducmanhphan93@gmail.com>
2019-12-03 22:40:23 +0700

    update files

--- src/libs/Redis/Jedis-library/basic-jedis/src/main/java/com/manhpd/connection/RedisConnection.java
+++ src/libs/Redis/Jedis-library/basic-jedis/src/main/java/com/manhpd/connection/RedisConnection.java
@@ -10,7 +10,9 @@ import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisSentinelPool;
 import redis.clients.util.Pool;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class RedisConnection {
@@ -51,67 +53,20 @@ public class RedisConnection {
 
     private void initPool() {
         // 1st way
-        this.pool = new JedisPool(Constants.FIXED_HOST, Constants.FIXED_PORT);
+//        this.pool = new JedisPool(Constants.FIXED_HOST, Constants.FIXED_PORT);
 
         // 2nd way
-//        try {
-//            GenericObjectPoolConfig genericObjectPoolConfig = new GenericObjectPoolConfig();
-//            genericObjectPoolConfig.setMaxWaitMillis(redisConfig.getRedisMaxWaitMs());
-//            genericObjectPoolConfig.setMaxTotal(redisConfig.getRedisMaxTotal());
-//            genericObjectPoolConfig.setMaxIdle(redisConfig.getRedisMaxIdle());
-//            genericObjectPoolConfig.setMinIdle(redisConfig.getRedisMinIdle());
-//            genericObjectPoolConfig.setNumTestsPerEvictionRun(redisConfig.getRedisNumTestsPerEvictionRun());
-//            genericObjectPoolConfig.setTimeBetweenEvictionRunsMillis(redisConfig.getRedisTimeBetweenEvictionRunMs());
-//            genericObjectPoolConfig.setMinEvictableIdleTimeMillis(redisConfig.getRedisMinEvictableIdleTimeMs());
-//            genericObjectPoolConfig.setTestOnBorrow(true);
-//            genericObjectPoolConfig.setTestOnReturn(true);
-//            genericObjectPoolConfig.setTestWhileIdle(true);
-//            genericObjectPoolConfig.setBlockWhenExhausted(true);
-//
-//            if ((redisConfig.getRedisMasterName() != null) && !"".equals(redisConfig.getRedisMasterName())) {
-//                String[] str = redisConfig.getRedisUrl().split(",");
-//                Set<String> sentinel = new HashSet<String>();
-//
-//                for (int i = 0; i < str.length; i++) {
-//                    if (str[i].contains(":")) {
-//                        int index = str[i].indexOf(":");
-//                        String redisHost = str[i].substring(0, index).trim();
-//                        String redisPort = str[i].substring(index + 1).trim();
-//                        sentinel.add(new HostAndPort(redisHost, Integer.parseInt(redisPort)).toString());
-//                    }
-//                }
-//
-//                pool = new JedisSentinelPool(
-//                        redisConfig.getRedisMasterName(),
-//                        sentinel,
-//                        genericObjectPoolConfig,
-//                        (int) redisConfig.getRedisTimeout(),
-//                        redisConfig.getRedisPass(),
-//                        redisConfig.getRedisDb()
-//                );
-//            } else {
-//                String str = redisConfig.getRedisUrl();
-//                String redisHost = "";
-//                String redisPort = "";
-//
-//                if (str.contains(":")) {
-//                    int index = str.indexOf(":");
-//                    redisHost = str.substring(0, index).trim();
-//                    redisPort = str.substring(index + 1).trim();
-//                }
-//
-//                pool = new JedisPool(
-//                        genericObjectPoolConfig,
-//                        redisHost,
-//                        Integer.parseInt(redisPort),
-//                        (int) redisConfig.getRedisTimeout(),
-//                        redisConfig.getRedisPass(),
-//                        redisConfig.getRedisDb()
-//                );
-//            }
-//        } catch (Exception ex) {
-//            System.out.println(ex.toString());
-//        }
+        try {
+            GenericObjectPoolConfig genericObjectPoolConfig = this.createPool(redisConfig);
+
+            if ((redisConfig.getRedisMasterName() != null) && !"".equals(redisConfig.getRedisMasterName())) {
+                this.createRedisSentinel(genericObjectPoolConfig);
+            } else {
+                this.createRedisPool(genericObjectPoolConfig);
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+        }
     }
 
     private void resetPool() {
@@ -144,6 +99,81 @@ public class RedisConnection {
                 System.out.println(e.toString());
             }
         }
+    }
+
+    private GenericObjectPoolConfig createPool(RedisConfig redisConfig) {
+        GenericObjectPoolConfig genericObjectPoolConfig = new GenericObjectPoolConfig();
+        genericObjectPoolConfig.setMaxWaitMillis(redisConfig.getRedisMaxWaitMs());
+        genericObjectPoolConfig.setMaxTotal(redisConfig.getRedisMaxTotal());
+        genericObjectPoolConfig.setMaxIdle(redisConfig.getRedisMaxIdle());
+        genericObjectPoolConfig.setMinIdle(redisConfig.getRedisMinIdle());
+        genericObjectPoolConfig.setNumTestsPerEvictionRun(redisConfig.getRedisNumTestsPerEvictionRun());
+        genericObjectPoolConfig.setTimeBetweenEvictionRunsMillis(redisConfig.getRedisTimeBetweenEvictionRunMs());
+        genericObjectPoolConfig.setMinEvictableIdleTimeMillis(redisConfig.getRedisMinEvictableIdleTimeMs());
+        genericObjectPoolConfig.setTestOnBorrow(true);
+        genericObjectPoolConfig.setTestOnReturn(true);
+        genericObjectPoolConfig.setTestWhileIdle(true);
+        genericObjectPoolConfig.setBlockWhenExhausted(true);
+
+        return genericObjectPoolConfig;
+    }
+
+    private Set<String> getSentinelFromUrl(String redisUrl) {
+        String[] str = redisUrl.split(",");
+        Set<String> sentinel = new HashSet<String>();
+
+        for (int i = 0; i < str.length; i++) {
+            if (str[i].contains(":")) {
+                int index = str[i].indexOf(":");
+                String redisHost = str[i].substring(0, index).trim();
+                String redisPort = str[i].substring(index + 1).trim();
+                sentinel.add(new HostAndPort(redisHost, Integer.parseInt(redisPort)).toString());
+            }
+        }
+
+        return sentinel;
+    }
+
+    private Map<String, String> getHostPort(String redisUrl) {
+        Map<String, String> result = new HashMap<>();
+        if (redisUrl.contains(":")) {
+            int index = redisUrl.indexOf(":");
+            String redisHost = redisUrl.substring(0, index).trim();
+            String redisPort = redisUrl.substring(index + 1).trim();
+
+            result.put(redisHost, redisPort);
+        }
+
+        return result;
+    }
+
+    private void createRedisSentinel(GenericObjectPoolConfig genericObjectPoolConfig) {
+        Set<String> sentinel = this.getSentinelFromUrl(redisConfig.getRedisUrl());
+        this.pool = new JedisSentinelPool(redisConfig.getRedisMasterName(),
+                sentinel,
+                genericObjectPoolConfig,
+                (int) redisConfig.getRedisTimeout(),
+                redisConfig.getRedisPass(),
+                redisConfig.getRedisDb());
+    }
+
+    private void createRedisPool(GenericObjectPoolConfig genericObjectPoolConfig) {
+        String redisHost = "";
+        String redisPort = "";
+
+        Map<String, String> hostPortUrl = this.getHostPort(redisConfig.getRedisUrl());
+        if (hostPortUrl.size() > 0) {
+            Map.Entry<String, String> entry = hostPortUrl.entrySet().iterator().next();
+            redisHost = entry.getKey();
+            redisPort = entry.getValue();
+        }
+
+        this.pool = new JedisPool(genericObjectPoolConfig,
+                                redisHost,
+                                Integer.parseInt(redisPort),
+                                (int) redisConfig.getRedisTimeout(),
+                                redisConfig.getRedisPass(),
+                                redisConfig.getRedisDb());
     }
 
 }

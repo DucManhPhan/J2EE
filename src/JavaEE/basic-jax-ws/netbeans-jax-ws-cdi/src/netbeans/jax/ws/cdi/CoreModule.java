5ea1429a5fcbb427bdca647ee45e3aed2d1f45c4
ManhPD5 <ducmanhphan93@gmail.com>
2020-01-12 19:29:11 +0700

    update files

--- src/JavaEE/basic-jax-ws/netbeans-jax-ws-cdi/src/netbeans/jax/ws/cdi/CoreModule.java
+++ src/JavaEE/basic-jax-ws/netbeans-jax-ws-cdi/src/netbeans/jax/ws/cdi/CoreModule.java
@@ -3,9 +3,11 @@ package netbeans.jax.ws.cdi;
 
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
+import javax.xml.ws.Endpoint;
 import netbeans.jax.ws.cdi.service.SimpleEvent;
 import netbeans.jax.ws.cdi.service.StartupService;
 import netbeans.jax.ws.cdi.service.TmpService;
+import netbeans.jax.ws.cdi.webservice.WsReceiver;
 import org.apache.log4j.Logger;
 
 public class CoreModule {
@@ -16,10 +18,13 @@ public class CoreModule {
     @Inject
     private TmpService tmpService;
     
-    public void onEvent(@Observes SimpleEvent ignored, StartupService service) {
+    public void onEvent(@Observes SimpleEvent ignored, StartupService startupService) {
         logger.info("Raise events in CoreModule");
         this.tmpService.displayData();
-        service.sayHello();
+        startupService.sayHello();
+
+        WsReceiver receiver = new WsReceiver();
+        Endpoint service = Endpoint.publish("http://localhost:9901/wsreceiver", receiver);
     }
 
 }
--- src/JavaEE/basic-jax-ws/netbeans-jax-ws-cdi/src/netbeans/jax/ws/cdi/NetbeansJaxWsCdi.java
+++ src/JavaEE/basic-jax-ws/netbeans-jax-ws-cdi/src/netbeans/jax/ws/cdi/NetbeansJaxWsCdi.java
@@ -2,9 +2,14 @@ package netbeans.jax.ws.cdi;
 
 import javax.enterprise.inject.se.SeContainer;
 import javax.enterprise.inject.se.SeContainerInitializer;
+import javax.inject.Inject;
 import javax.xml.ws.Endpoint;
+import netbeans.jax.ws.cdi.service.MyService;
+import netbeans.jax.ws.cdi.service.MyServiceImpl;
 import netbeans.jax.ws.cdi.service.SimpleEvent;
+import netbeans.jax.ws.cdi.service.TmpService;
 import netbeans.jax.ws.cdi.webservice.WsReceiver;
+import org.apache.log4j.Logger;
 
 /**
  *
@@ -13,12 +18,15 @@ import netbeans.jax.ws.cdi.webservice.WsReceiver;
 public class NetbeansJaxWsCdi {
 
     public static void main(String[] args) {
-        WsReceiver receiver = new WsReceiver();
-        Endpoint service = Endpoint.publish("http://localhost:9901/wsreceiver", receiver);
-
-        // Scan beans in our project
         SeContainerInitializer initializer = SeContainerInitializer.newInstance();
-        SeContainer container = initializer.addPackages(Package.getPackage("netbeans.jax.ws.cdi")).initialize();
+        // addPackages(Package.getPackage("netbeans.jax.ws.cdi")).
+//        SeContainer container = initializer.initialize();
+        SeContainer container = initializer.addPackages(true, CoreModule.class).disableDiscovery().initialize();
+//        TmpService tmpService = container.select(TmpService.class).get();
+//        tmpService.displayData();
+        
+//        MyService myService = container.select(MyServiceImpl.class).get();
+//        myService.sayHello("Everybody");
         container.getBeanManager().fireEvent(new SimpleEvent());
         
         container.close();

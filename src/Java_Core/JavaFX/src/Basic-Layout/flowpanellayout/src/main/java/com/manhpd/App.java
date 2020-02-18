6ff7f76b8eb4cad40b7a9adb16a32572d6cab59c
ManhPD5 <ducmanhphan93@gmail.com>
2020-02-18 16:52:24 +0700

    update files

--- src/Java_Core/JavaFX/src/Basic-Layout/flowpanellayout/src/main/java/com/manhpd/App.java
+++ src/Java_Core/JavaFX/src/Basic-Layout/flowpanellayout/src/main/java/com/manhpd/App.java
@@ -17,7 +17,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary.fxml"), 640, 480);
+        this.scene = new Scene(loadFXML("primary.fxml"), 640, 480);
+        stage.setTitle("Example about showing date time");
         stage.setScene(scene);
         stage.show();
     }
@@ -27,7 +28,7 @@ public class App extends Application {
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml));
         return fxmlLoader.load();
     }
 
--- src/Java_Core/JavaFX/src/Basic-Layout/flowpanellayout/src/main/java/com/manhpd/PrimaryController.java
+++ src/Java_Core/JavaFX/src/Basic-Layout/flowpanellayout/src/main/java/com/manhpd/PrimaryController.java
@@ -1,12 +1,54 @@
 package com.manhpd;
 
 import java.io.IOException;
-import javafx.fxml.FXML;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
 
-public class PrimaryController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class PrimaryController implements Initializable {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        System.out.println("Switch to secondary secene");
+    private Button btnShowDateTime;
+
+    @FXML
+    private TextField txtDateTime;
+
+    @FXML
+    private void showDateTime() throws IOException {
+        System.out.println("show-date-time button clicked");
+        LocalDateTime now = LocalDateTime.now();
+
+        // yyyy-MM-dd HH:mm:ss
+        // dd/LLLL/yyyy HH:mm:ss
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String dateTimeNow = formatter.format(now);
+
+        this.txtDateTime.setText(dateTimeNow);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // it can do nothing
+    }
+
+    public void isOk(ActionEvent actionEvent) {
+
+    }
+
+    public void displayName(ActionEvent actionEvent) {
+    }
+
+    public void clickRdBtn(ActionEvent actionEvent) {
+    }
+
+    public void clickCheckbox(ActionEvent actionEvent) {
     }
 }

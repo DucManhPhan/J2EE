c1328a27031bf2a41845e5a0fe702debcdbc31fd
DucManhPhan <ducmanhphan93@gmail.com>
2020-02-21 01:04:44 +0700

    update files

--- src/Utils/httpclient-utils/src/main/java/com/manhpd/HttpClientUtils.java
+++ src/Utils/httpclient-utils/src/main/java/com/manhpd/HttpClientUtils.java
@@ -1,7 +1,24 @@
 package com.manhpd;
 
+import com.manhpd.utils.Constant;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+
 public class HttpClientUtils {
 
+    public static void getRequest() throws IOException {
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpResponse response = client.execute(new HttpGet(Constant.url));
+        int statusCode = response.getStatusLine().getStatusCode();
+        System.out.println("Status of response code is: " + statusCode);
+    }
 
+    public static void postRequest() {
+
+    }
 
 }
---
+++ src/Utils/httpclient-utils/src/main/java/com/manhpd/utils/Constant.java
@@ -0,0 +1,7 @@
+package com.manhpd.utils;
+
+public class Constant {
+
+    public static final String url = "http://dev.lifesup.com.vn:9999";
+
+}

51dde1888579da898fb0d0b7cf07bde983c1e114
unknown <ducmanhphan93@gmail.com>
2020-07-30 17:36:42 +0700

    update files

---
+++ src/Java_Spring/Spring-Boot/cdc-debezium/src/main/java/com/manhpd/debezium/CdcDebeziumApplication.java
@@ -0,0 +1,13 @@
+package com.manhpd.debezium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdcDebeziumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdcDebeziumApplication.class, args);
+    }
+
+}

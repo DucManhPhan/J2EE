a9dfa35edbd95747d1d025199d9f37ab36f8e8d5
Phan Manh <ducmanhphan93@gmail.com>
2020-08-03 23:25:00 +0700

    update files

--- src/Java_Spring/Spring-Boot/cdc-debezium/src/main/java/com/manhpd/debezium/config/debezium/DebeziumConfig.java
+++ src/Java_Spring/Spring-Boot/cdc-debezium/src/main/java/com/manhpd/debezium/config/debezium/DebeziumConfig.java
@@ -10,6 +10,17 @@ public class DebeziumConfig {
     public io.debezium.config.Configuration employeeConnector() {
         return io.debezium.config.Configuration.create()
                 .with("connector.class", "io.debezium.connector.mysql.MySQLConnector")
+                .with("offset.storage",  "org.apache.kafka.connect.storage.FileOffsetBackingStore")
+                .with("offset.storage.file.filename", "/path/cdc/offset/student-offset.dat")
+                .with("offset.flush.interval.ms", 60000)
+                .with("name", "student-postgres-connector")
+                .with("database.server.name", studentDBHost + "-" + studentDBName)
+                .with("database.hostname", studentDBHost)
+                .with("database.port", studentDBPort)
+                .with("database.user", studentDBUserName)
+                .with("database.password", studentDBPassword)
+                .with("database.dbname", studentDBName)
+                .with("table.whitelist", STUDENT_TABLE_NAME).build();
     }
 
 }

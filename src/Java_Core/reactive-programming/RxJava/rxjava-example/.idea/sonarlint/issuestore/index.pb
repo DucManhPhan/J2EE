fc45736b244578682ca00ba927be2e6119c63b4c
manhpd <ducmanhphan93@gmail.com>
2021-04-23 17:34:36 +0700

    update files

---
+++ src/Java_Core/reactive-programming/RxJava/rxjava-example/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+Q
+!src/main/java/com/manhpd/App.java,f/8/f88aa157fe7c68b9899e081e7ae75748fdf98a4f
--- src/Java_Core/reactive-programming/RxJava/rxjava-example/src/main/java/com/manhpd/App.java
+++ src/Java_Core/reactive-programming/RxJava/rxjava-example/src/main/java/com/manhpd/App.java
@@ -1,12 +1,18 @@
 package com.manhpd;
 
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.observers.DisposableObserver;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class App {
     public static void main(String[] args) {
-        useIntermediateOperator();
+//        useIntermediateOperator();
+//        useFromIterable();
+        defineCustomeObserver();
     }
 
     public static void streamData() {
@@ -20,6 +26,13 @@ public class App {
         secondIntervals.subscribe(s -> System.out.println(s));
 
         sleep(5000);
+    }
+
+    public static void useFromIterable() {
+        List<String> strs = Arrays.asList("alpha", "beta", "gamma");
+        Observable<String> source = Observable.fromIterable(strs);
+
+        source.subscribe(s -> System.out.println(s));
     }
 
     private static void sleep(long millis) {
@@ -63,4 +76,37 @@ public class App {
         filtered.subscribe(s -> System.out.println("RECEIVED: " + s));
     }
 
+    public static void useDisposable() {
+        Disposable d = Observable.just("Hello world!")
+                .delay(1, TimeUnit.SECONDS)
+                .subscribeWith(new DisposableObserver<String>() {
+                    @Override public void onStart() {
+                        System.out.println("Start!");
+                    }
+                    @Override public void onNext(String t) {
+                        System.out.println(t);
+                    }
+                    @Override public void onError(Throwable t) {
+                        t.printStackTrace();
+                    }
+                    @Override public void onComplete() {
+                        System.out.println("Done!");
+                    }
+                });
+
+        sleep(500);
+        // the sequence can now be disposed via dispose()
+        d.dispose();
+    }
+
+    public static void defineCustomeObserver() {
+        Observable<String> source = Observable.just("alpha", "beta", "gamma");
+        source.map(String::length)
+                .filter(i -> i >= 5)
+                .subscribe(i -> System.out.println(i),
+                        Throwable::printStackTrace,
+                        () -> System.out.println("Done!"));
+    }
+
 }
+

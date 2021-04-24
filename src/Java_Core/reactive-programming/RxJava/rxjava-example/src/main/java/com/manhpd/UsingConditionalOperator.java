e2e6578e8318a0274779cab3eec41fb0eedfc570
Phan Manh <ducmanhphan93@gmail.com>
2021-04-24 16:36:09 +0700

    update files

---
+++ src/Java_Core/reactive-programming/RxJava/rxjava-example/src/main/java/com/manhpd/UsingConditionalOperator.java
@@ -0,0 +1,23 @@
+package com.manhpd;
+
+import io.reactivex.rxjava3.core.Observable;
+
+public class UsingConditionalOperator {
+
+    public static void main(String[] args) {
+        usingTakeWhile();
+    }
+
+    /**
+     * When the value of a condition from takeWhile() method is false at the first time, onComplete event
+     * will be pushed to the Observer, and disposed the used resources.
+     *
+     * Therefore, if we change the condition i < 5 to i > 5, no emissions will be fired.
+     */
+    public static void usingTakeWhile() {
+        Observable.range(1, 100)
+                .takeWhile(i -> i > 5)
+                .subscribe(s -> System.out.println("RECEIVED: " + s));
+    }
+
+}

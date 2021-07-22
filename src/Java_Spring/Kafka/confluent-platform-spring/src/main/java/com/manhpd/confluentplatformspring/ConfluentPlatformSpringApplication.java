c4672fc9ac30a30c7dc1a4007200b5226473ad33
Minh <ducmanhphan93@gmail.com>
2021-07-22 13:33:09 +0700

    update files

---
+++ src/Java_Spring/Kafka/confluent-platform-spring/src/main/java/com/manhpd/confluentplatformspring/ConfluentPlatformSpringApplication.java
@@ -0,0 +1,13 @@
+package com.manhpd.confluentplatformspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfluentPlatformSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfluentPlatformSpringApplication.class, args);
+    }
+
+}
---
+++ src/Java_Spring/Kafka/confluent-platform-spring/src/test/java/com/manhpd/confluentplatformspring/ConfluentPlatformSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manhpd.confluentplatformspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfluentPlatformSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

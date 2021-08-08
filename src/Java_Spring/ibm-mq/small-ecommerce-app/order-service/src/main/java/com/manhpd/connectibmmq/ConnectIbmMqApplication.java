762d43f6723039dd881e8c436c9928c55d2de326
Phan Manh <ducmanhphan93@gmail.com>
2021-08-08 18:06:42 +0700

    update files

---
+++ src/Java_Spring/ibm-mq/small-ecommerce-app/order-service/src/main/java/com/manhpd/connectibmmq/ConnectIbmMqApplication.java
@@ -0,0 +1,13 @@
+package com.manhpd.connectibmmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectIbmMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectIbmMqApplication.class, args);
+    }
+
+}
---
+++ src/Java_Spring/ibm-mq/small-ecommerce-app/order-service/src/test/java/com/manhpd/connectibmmq/ConnectIbmMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manhpd.connectibmmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectIbmMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

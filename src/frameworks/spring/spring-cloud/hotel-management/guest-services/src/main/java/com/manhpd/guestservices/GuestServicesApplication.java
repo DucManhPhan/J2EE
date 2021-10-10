1502910264d5e5d118238c7b556ed6bc246be2d5
Phan Manh <ducmanhphan93@gmail.com>
2021-10-10 21:36:16 +0700

    add hotel-management project

---
+++ src/frameworks/spring/spring-cloud/hotel-management/guest-services/src/main/java/com/manhpd/guestservices/GuestServicesApplication.java
@@ -0,0 +1,13 @@
+package com.manhpd.guestservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestServicesApplication.class, args);
+	}
+
+}

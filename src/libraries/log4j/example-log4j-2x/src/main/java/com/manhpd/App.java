a9574d8d1ae5b30da46253f07067880413e6079b
Manh. Phan Duc  - CMCGlobal DU19 <ducmanhphan93@gmail.com>
2021-12-16 09:25:56 +0700

    update files

--- src/libraries/log4j/example-log4j-2x/src/main/java/com/manhpd/App.java
+++ src/libraries/log4j/example-log4j-2x/src/main/java/com/manhpd/App.java
@@ -1,18 +1,20 @@
 package com.manhpd;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+//import org.apache.logging.log4j.LogManager;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    private static Logger logger = LogManager.getLogger(App.class);
+public class App {
+//    private static Logger logger = LogManager.getLogger(App.class);
 
-    public static void main( String[] args ) throws InterruptedException {
-        for(int i = 0;; i++) {
+    private final static Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; ; i++) {
             System.out.println("Logging.\n");
             logger.info("This is the " + i + " time I say 'Hello World'.");
             Thread.sleep(100);

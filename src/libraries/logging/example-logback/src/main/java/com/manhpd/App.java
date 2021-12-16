2bee1b5947222f20fb8b4a4c432a6bf301b6a1c7
Phan Manh <ducmanhphan93@gmail.com>
2021-12-16 21:56:10 +0700

    update files

--- src/libraries/logging/example-logback/src/main/java/com/manhpd/App.java
+++ src/libraries/logging/example-logback/src/main/java/com/manhpd/App.java
@@ -1,13 +1,14 @@
 package com.manhpd;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello World!");
+        logger.info("Test logback library");
     }
 }

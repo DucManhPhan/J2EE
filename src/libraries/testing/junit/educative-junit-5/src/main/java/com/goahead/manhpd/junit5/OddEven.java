ee3881cbf33b42e41f458ded112cb0f3a930a7c6
unknown <ducmanhphan93@gmail.com>
2022-02-04 00:21:04 +0700

    update files

--- src/libraries/testing/junit/educative-junit-5/src/main/java/com/goahead/manhpd/junit5/OddEven.java
+++ src/libraries/testing/junit/educative-junit-5/src/main/java/com/goahead/manhpd/junit5/OddEven.java
@@ -1,4 +1,9 @@
 package com.goahead.manhpd.junit5;
 
 public class OddEven {
+
+    public boolean isNumberEven(int number) {
+        return number % 2 == 0;
+    }
+
 }
--- src/libraries/testing/junit/educative-junit-5/src/test/java/com/goahead/manhpd/junit5/OddEvenTest.java
+++ src/libraries/testing/junit/educative-junit-5/src/test/java/com/goahead/manhpd/junit5/OddEvenTest.java
@@ -1,5 +1,21 @@
+package com.goahead.manhpd.junit5;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class OddEvenTest {
+
+    @Test
+    void givenEvenNumber_whenIsEvenIsCalled_thenTrueIsReturned() {
+        OddEven oddEven = new OddEven();
+        assertTrue(oddEven.isNumberEven(10));
+    }
+
+    @Test
+    void givenOddNumber_whenIsEvenIsCalled_thenFalseIsReturned() {
+        OddEven oddEven = new OddEven();
+        assertFalse(oddEven.isNumberEven(11));
+    }
 
 }

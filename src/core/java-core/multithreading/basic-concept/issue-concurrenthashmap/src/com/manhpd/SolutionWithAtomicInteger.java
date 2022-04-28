212eca52b6c091a365e4715c09f2530ae41cc3ff
Phan Manh <ducmanhphan93@gmail.com>
2022-04-29 05:21:08 +0700

    update files

--- src/core/java-core/multithreading/basic-concept/issue-concurrenthashmap/src/com/manhpd/SolutionWithAtomicInteger.java
+++ src/core/java-core/multithreading/basic-concept/issue-concurrenthashmap/src/com/manhpd/SolutionWithAtomicInteger.java
@@ -1,4 +1,34 @@
 package com.manhpd;
 
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class SolutionWithAtomicInteger {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ConcurrentHashMap<String, AtomicInteger> map = new ConcurrentHashMap<>();
+        map.put("value", new AtomicInteger(0));
+
+        ExecutorService es = Executors.newFixedThreadPool(5);
+
+        Runnable task = () -> {
+            for (int i = 0; i < 100; ++i) {
+                map.get("value").incrementAndGet();
+            }
+        };
+
+        // submit the task twice
+        Future future1 = es.submit(task);
+        Future future2 = es.submit(task);
+
+        // wait for the threads to finish
+        future1.get();
+        future2.get();
+
+        // shutdown the executor service
+        es.shutdown();
+
+        System.out.println("The value: " + map.get("value"));
+    }
+
 }

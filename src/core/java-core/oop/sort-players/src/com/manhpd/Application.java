c9bfda32537db9852750c4a041754a51fc849640
Phan Manh <ducmanhphan93@gmail.com>
2022-05-08 16:43:54 +0700

    updat efiles

--- src/core/java-core/oop/sort-players/src/com/manhpd/Application.java
+++ src/core/java-core/oop/sort-players/src/com/manhpd/Application.java
@@ -41,7 +41,7 @@ public class Application {
 
         Comparator<Player> byIncreasedRanking = (p1, p2) -> {
             if (p1.getRanking() != p2.getRanking()) {
-                return Integer.compare(p1.getRanking(), p2.getRanking());
+                return Integer.compare(p2.getRanking(), p1.getRanking());
             } else {
                 return Integer.compare(p1.getLogicalTimer(), p2.getLogicalTimer());
             }
@@ -59,7 +59,33 @@ public class Application {
         System.out.println(playersByRanking.poll().toString());
     }
 
+    /**
+     * The lowest age will be at the 0 index.
+     * If the players has the same age, based on the insertion order.
+     */
     private static void sortPriorityQueueByDecreasedAge() {
+        Player player1 = new Player(59, "John", 20, ++counter);
+        Player player2 = new Player(67, "Roger1", 22, ++counter);
+        Player player3 = new Player(67, "Roger2", 22, ++counter);
+        Player player4 = new Player(45, "Steven", 24, ++counter);
 
+        Comparator<Player> byIncreasedRanking = (p1, p2) -> {
+            if (p1.getAge() != p2.getAge()) {
+                return Integer.compare(p1.getAge(), p2.getAge());
+            } else {
+                return Integer.compare(p1.getLogicalTimer(), p2.getLogicalTimer());
+            }
+        };
+
+        PriorityQueue<Player> playersByRanking = new PriorityQueue<>(byIncreasedRanking);
+        playersByRanking.offer(player1);
+        playersByRanking.offer(player2);
+        playersByRanking.offer(player3);
+        playersByRanking.offer(player4);
+
+        System.out.println(playersByRanking.poll().toString());
+        System.out.println(playersByRanking.poll().toString());
+        System.out.println(playersByRanking.poll().toString());
+        System.out.println(playersByRanking.poll().toString());
     }
 }

7560d5464239b4d27be67f581105e70f4e28c6e7
pdmanh <ducmanhphan93@gmail.com>
2022-05-13 17:35:15 +0700

    update files

---
+++ src/frameworks/spring/spring-security/spring-security-jwt/src/main/java/com/manhpd/ssecurityjwt/SsecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.manhpd.ssecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsecurityJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsecurityJwtApplication.class, args);
+    }
+
+}
---
+++ src/frameworks/spring/spring-security/spring-security-jwt/src/test/java/com/manhpd/ssecurityjwt/SsecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manhpd.ssecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsecurityJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

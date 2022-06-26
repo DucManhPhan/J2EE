ce16cbcc06403a534f9c06f9b0f3d2a0ee05ae85
Phan Manh <ducmanhphan93@gmail.com>
2022-06-26 23:00:08 +0700

    update files

---
+++ src/libraries/testing/junit/book-management/README.md
@@ -0,0 +1,2 @@
+
+https://viblo.asia/p/viet-unit-test-service-layer-trong-spring-boot-voi-junit-5-bJzKmreBZ9N
---
+++ src/libraries/testing/junit/book-management/src/test/java/com/manhpd/bookmanagement/BookManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manhpd.bookmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

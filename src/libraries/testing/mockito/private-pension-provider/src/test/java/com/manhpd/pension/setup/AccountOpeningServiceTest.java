336d1fe2216684089b2551a890f86056812409ab
manhpd <ducmanhphan93@gmail.com>
2022-10-07 05:50:42 +0700

    update files

--- src/libraries/testing/mockito/private-pension-provider/src/test/java/com/manhpd/pension/setup/AccountOpeningServiceTest.java
+++ src/libraries/testing/mockito/private-pension-provider/src/test/java/com/manhpd/pension/setup/AccountOpeningServiceTest.java
@@ -1,32 +1,33 @@
 package com.manhpd.pension.setup;
 
 import com.manhpd.pension.AccountRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 class AccountOpeningServiceTest {
 
-    private AccountOpeningService underTest;
+    private AccountOpeningService accountOpeningService;
     private BackgroundCheckService backgroundCheckService = mock(BackgroundCheckService.class);
     private ReferenceIdsManager referenceIdsManager = mock(ReferenceIdsManager.class);
     private AccountRepository accountRepository = mock(AccountRepository.class);
 
     @BeforeEach
     void setUp() {
-        underTest = new AccountOpeningService(backgroundCheckService, referenceIdsManager, accountRepository);
+        this.accountOpeningService =
+                new AccountOpeningService(this.backgroundCheckService, this.referenceIdsManager, this.accountRepository);
     }
 
     @Test
     public void shouldOpenAccount() throws IOException {
-        final AccountOpeningStatus accountOpeningStatus = underTest.openAccount("John", "Smith", "123XYZ9", LocalDate.of(1990, 1, 1));
-        assertEquals(AccountOpeningStatus.OPENED, accountOpeningStatus);
+        final AccountOpeningStatus accountOpeningStatus =
+                this.accountOpeningService.openAccount("John", "Smith", "123XYZ9",
+                                                        LocalDate.of(1990, 1, 1));
+        Assertions.assertEquals(AccountOpeningStatus.OPENED, accountOpeningStatus);
     }
 }

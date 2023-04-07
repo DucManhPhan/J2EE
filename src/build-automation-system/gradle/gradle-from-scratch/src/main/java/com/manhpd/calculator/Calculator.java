5eb16f89d75d76882f9e0dd40d9be717bfa993cc
unknown <ducmanhphan93@gmail.com>
2023-04-07 22:25:30 +0700

    update files

---
+++ src/build-automation-system/gradle/gradle-from-scratch/src/main/java/com/manhpd/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package main.java.com.manhpd.calculator;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        return a / b;
+    }
+}
---
+++ src/build-automation-system/gradle/gradle-from-scratch/src/main/java/com/manhpd/calculator/Main.java
@@ -0,0 +1,25 @@
+package main.java.com.manhpd.calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.err.println("Need to provide 3 arguments: [operation] [value 1] [value 2]");
+            System.exit(0);
+        }
+
+        String operation = args[0];
+        Integer a = Integer.valueOf(args[1]);
+        Integer b = Integer.valueOf(args[2]);
+
+        Calculator calculator = new Calculator();
+        if ("add".equals(operation)) {
+            System.out.println(calculator.add(a, b));
+        } else if ("subtract".equals(operation)) {
+            System.out.println(calculator.subtract(a, b));
+        } else if ("multiply".equals(operation)) {
+            System.out.println(calculator.multiply(a, b));
+        } else {
+            System.out.println(calculator.divide(a, b));
+        }
+    }
+}

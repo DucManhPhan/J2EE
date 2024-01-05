301ded3b8ac02771d2201da1a244c00b25fdd3db
manhpd <ducmanhphan93@gmail.com>
2024-01-05 18:20:49 +0700

    update files

---
+++ src/libraries/testing/spring-testing/kafka-testing/src/main/java/com/manhpd/kafkatesting/KafkaTestingApplication.java
@@ -0,0 +1,13 @@
+package com.manhpd.kafkatesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaTestingApplication.class, args);
+    }
+
+}
